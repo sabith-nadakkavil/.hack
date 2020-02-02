85948c2500d621166ad78575073df7fdc96310d1
sabith-nadakkavil <sabithnadakkavil@gmail.com>
2020-02-02 12:38:21 +0530

    Hack for .hack

---
+++ flutt/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+            Text('P-Learn',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0,)),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 25.0),
+                width: double.infinity,
+                child: RaisedButton(
+                  elevation: 5.0,
+                  onPressed: () => print('Login Button Pressed'),
+                  padding: EdgeInsets.all(15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  color: Colors.blue,
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 1.5,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'OpenSans',
+                    ),
+                  ),
+                ),
+              ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 25.0),
+          width: double.infinity,
+          child: RaisedButton(
+            elevation: 5.0,
+            onPressed: () => print('Register Button Pressed'),
+            padding: EdgeInsets.all(15.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: Colors.blue,
+            child: Text(
+              'REGISTER',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'OpenSans',
+              ),
+            ),
+          ),
+        )
+        ],
+      ),
+    )
+    );
+  }
+}
